251f34ba81e9793e60b0fdd350659e88b3f6648d
Siddharth <sidp5276@gmail.com>
2026-06-12 09:57:31 +0530

    API testing GET Bdd Style

---
+++ src/test/java/com/ApiFramework/ex03_RestAssured_TestNG_Advance/GET/APITesting005_GET_BDDStyle.java
@@ -0,0 +1,35 @@
+package com.ApiFramework.ex03_RestAssured_TestNG_Advance.GET;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class APITesting005_GET_BDDStyle {
+
+    @Test
+    public void test_GET_POSITIVE(){
+        String pincode = "560058";
+        pincode = "110048";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+    }
+
+    @Test
+    public void test_GET_POSITIVE2(){
+        String pincode = "560058";
+        pincode = "110048";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+    }
+}
