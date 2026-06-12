da15509914ad9b5b26e35e40c0c997b5ee66e726
Siddharth <sidp5276@gmail.com>
2026-06-12 16:28:22 +0530

    API Testing 20 TestNG Invocation

---
+++ src/test/java/com/ApiFramework/ex_04_TestNGAdvanceExample/APITesting020_TestNG_invocationCount.java
@@ -0,0 +1,16 @@
+package com.ApiFramework.ex_04_TestNGAdvanceExample;
+
+import org.testng.annotations.Test;
+
+public class APITesting020_TestNG_invocationCount {
+
+    @Test(invocationCount = 2)
+    public void test01(){
+        System.out.println("Hi");
+    }
+
+    @Test(invocationCount = 3)
+    public void test02(){
+        System.out.println("Bye");
+    }
+}
