2299a507665d5b92b559c08b20fb7362e5ad4645
Siddharth <sidp5276@gmail.com>
2026-06-12 12:10:48 +0530

    API testing 12 TestNG basics

---
+++ src/test/java/com/ApiFramework/ex_04_TestNGAdvanceExample/APITesting012_TestNG_Basics.java
@@ -0,0 +1,15 @@
+package com.ApiFramework.ex_04_TestNGAdvanceExample;
+
+import org.testng.annotations.Test;
+
+public class APITesting012_TestNG_Basics {
+
+    public void test_tc1(){
+
+    }
+
+    @Test
+    public void test_tc2(){
+
+    }
+}
