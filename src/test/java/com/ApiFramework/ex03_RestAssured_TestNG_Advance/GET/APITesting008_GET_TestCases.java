9284ef2684c86c22cc913990fe0ea979abdade8a
Siddharth <sidp5276@gmail.com>
2026-06-12 10:10:09 +0530

    API testing 8 GET Enable TestCases

---
+++ src/test/java/com/ApiFramework/ex03_RestAssured_TestNG_Advance/GET/APITesting008_GET_TestCases.java
@@ -0,0 +1,21 @@
+package com.ApiFramework.ex03_RestAssured_TestNG_Advance.GET;
+
+import org.testng.annotations.Test;
+
+public class APITesting008_GET_TestCases {
+
+    @Test
+    public void test_01(){
+
+    }
+
+    @Test(enabled = false)
+    public void test_02(){
+
+    }
+
+    @Test
+    public void test_03(){
+
+    }
+}
