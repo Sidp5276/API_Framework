171abfbae014991ee4d5c4013d8bfae07c107cee
Siddharth <sidp5276@gmail.com>
2026-06-13 09:21:07 +0530

    Person

---
+++ src/test/java/com/ApiFramework/ex_06_PayLoad_Management/Class/Tool/Person.java
@@ -0,0 +1,14 @@
+package com.ApiFramework.ex_06_PayLoad_Management.Class.Tool;
+
+public class Person {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
